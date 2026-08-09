d4c2f18a2a781b3388d6ba462d1d19d73608ab56
Harel_k <harelikatz@gmail.com>
2026-08-09 22:47:49 +0300

    fix: unregister integrations before shutdown

--- src/main/java/com/qducks/superducksystem/SuperDuckSystem.java
+++ src/main/java/com/qducks/superducksystem/SuperDuckSystem.java
@@ -77,6 +77,9 @@ public final class SuperDuckSystem extends JavaPlugin {
         if (moduleManager != null) {
             moduleManager.shutdown();
         }
+        if (integrationManager != null) {
+            integrationManager.shutdown();
+        }
         if (databaseManager != null) {
             databaseManager.close();
         }
