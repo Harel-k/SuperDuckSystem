a97f253a13c726de08561b1d7daab00ccc30eedf
Harel_k <harelikatz@gmail.com>
2026-08-09 22:53:15 +0300

    feat: add rewards command

---
+++ src/main/java/com/qducks/superducksystem/reward/RewardsCommand.java
@@ -0,0 +1,30 @@
+package com.qducks.superducksystem.reward;
+
+import com.qducks.superducksystem.SuperDuckSystem;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public final class RewardsCommand implements CommandExecutor {
+    private final RewardsMenu menu;
+
+    public RewardsCommand(SuperDuckSystem plugin, RewardService rewards) {
+        this.menu = new RewardsMenu(plugin, rewards);
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendRichMessage("<red>This command can only be used by players.</red>");
+            return true;
+        }
+        if (args.length != 0) {
+            player.sendRichMessage("<red>Usage: /rewards</red>");
+            return true;
+        }
+        menu.open(player);
+        return true;
+    }
+}
