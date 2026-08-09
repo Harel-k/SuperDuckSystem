0d2eda47d3ad77fed6ff0e33065442f4756fb4ad
Harel_k <harelikatz@gmail.com>
2026-08-09 19:16:01 +0300

    feat: add sell command

---
+++ src/main/java/com/qducks/superducksystem/shop/SellCommand.java
@@ -0,0 +1,29 @@
+package com.qducks.superducksystem.shop;
+
+import com.qducks.superducksystem.SuperDuckSystem;
+import com.qducks.superducksystem.message.MessageService;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public final class SellCommand implements CommandExecutor {
+    private final SellMenu menu;
+    private final MessageService messages;
+
+    public SellCommand(SuperDuckSystem plugin, ShopService shop) {
+        this.menu = new SellMenu(plugin, shop);
+        this.messages = new MessageService(plugin);
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player player)) {
+            messages.send(sender, "errors.players-only", "<red>This command can only be used by players.</red>");
+            return true;
+        }
+        menu.open(player);
+        return true;
+    }
+}
